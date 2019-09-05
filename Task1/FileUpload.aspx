e9a3f44b2ed73c02f5fa116899fe901ac604736a
Arathy Pillai <pillai.arathy95@hotmail.com>
2019-09-05 12:06:18 +0530

    Add project files.

---
+++ Task1/FileUpload.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FileUpload.aspx.cs" Inherits="Task1.FileUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Upload Image: </td><td><asp:FileUpload ID="uploadimage" runat="server" /></td>
+                </tr>
+                <tr>
+                        <td><asp:Button ID="btnsave" runat="server" OnClick="btnsave_Click" Text="UPLOAD IMAGE"/></td>
+                    </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Task1/FileUpload.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace Task1
+{
+    public partial class FileUpload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(Server.MapPath("images")))
+            {
+                Directory.CreateDirectory(Server.MapPath("images"));
+            }
+
+            string imgpath = Server.MapPath("images") + "\\" + uploadimage.FileName;
+            uploadimage.SaveAs(imgpath);
+        }
+    }
+}
